092f9a2c81dbabdcd7dbe473654cfdb975887ff4
Jitu <37040550+jitendraimrb@users.noreply.github.com>
2023-08-05 15:53:50 +0530

    Add files via upload

---
+++ src/main/java/com/learn/json/SpringbootOpenshiftAppTestApplication.java
@@ -0,0 +1,13 @@
+package com.learn.json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOpenshiftAppTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootOpenshiftAppTestApplication.class, args);
+	}
+
+}
